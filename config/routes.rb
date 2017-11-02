6203f72ac78c700fee9f65163cc7d4daf0dbfe6a
tomBeach <tbeach2k@gmail.com>
2017-11-02 08:44:25 -0400

    message functions mostly working

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,14 @@ Rails.application.routes.draw do
     get "/home" => "users#home"
 
     devise_for :users, :controllers => {:registrations => "registrations"}
-    resources :users, :only => [:index, :show, :edit, :update] do
+    resources :users do
         resources :messages
     end
     resources :photos
     resources :languages
     resources :neighborhoods
 
-    # resources :interests
-    # resources :places
-    #
-    # get "/search_local_places" => "users#search_local_places"
-    # get "/show_local_places" => "users#show_local_places"
+    get "/filter_messages" => "messages#filter_messages"
+    get "/my_messages/:id" => "messages#my_messages"
 
 end
