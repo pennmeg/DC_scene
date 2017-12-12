78726e96aa46941fd86e9cd0cd208f1a5a70cb26
Megan Penn <penn.meg@gmail.com>
2017-12-12 13:16:33 -0500

    updated photos path

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,7 +12,7 @@ class PhotosController < ApplicationController
       @photo = Photo.new(photo_params)
       if @photo.save
         flash[:success] = "The photo was added!"
-        redirect_to user_path(current_user[:id])
+        redirect_to photos_path
       else
         render 'new'
       end
